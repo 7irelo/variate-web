be73c3cfc5106b40bb05a8b9b382cd3f5bae6655
7irelo <tirelo.eric@gmail.com>
2024-06-18 16:46:23 +0200

    Update Product.cs

--- Variate/Models/Product.cs
+++ Variate/Models/Product.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 namespace Variate.Models;
 
 public class Product
 {
     [key] public int Id { get; set; }
+    [DisplayName("Category ID")]
     public int CategoryId { get; set; }
     public Category? Category{ get; set; }
     [Required][StringLength(50)] public string Name { get; set; }
