d6bcca04045a2c484c77bf58523de59b31e9dc52
7irelo <tirelo.eric@gmail.com>
2024-06-18 14:45:10 +0200

    Update ProductController.cs

--- Variate/Controllers/ProductController.cs
+++ Variate/Controllers/ProductController.cs
@@ -1,1 +1,32 @@
-namespace Variate.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Variate.Models;
+
+namespace Variate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
