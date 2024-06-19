a19731fb21c5d4ae13687176143fcc340349f62c
7irelo <tirelo.eric@gmail.com>
2024-06-19 14:11:34 +0200

    Create DataExtensions.cs

---
+++ Variate/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Variate.Data;
+
+public static class DataExtentions
+{
+    public static async Task MigrateDbAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+}
