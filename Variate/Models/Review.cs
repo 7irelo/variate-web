eba4798f88609c4e0ec8f66c5b44cdc17c3cb930
7irelo <tirelo.eric@gmail.com>
2024-06-19 15:03:01 +0200

    Create Review.cs

---
+++ Variate/Models/Review.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace Variate.Models;
+
+public class Product
+{
+    [key] public int Id { get; set; }
+    [DisplayName("Product ID")]
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+    [DisplayName("User ID")]
+    public int UserId { get; set; }
+    public User? User { get; set; }
+    public int Rating { get; set; }
+    [DisplayName("Comment")]
+    [Required][StringLength(200)]
+    public string ReviewComment { get; set; }
+}
