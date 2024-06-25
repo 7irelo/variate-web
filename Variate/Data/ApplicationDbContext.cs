0df361cb2e997805da836f7266bc8c5c6e33163d
7irelo <155379151+7irelo@users.noreply.github.com>
2024-06-25 17:04:23 +0200

    Update ApplicationDbContext.cs

--- Variate/Data/ApplicationDbContext.cs
+++ Variate/Data/ApplicationDbContext.cs
@@ -20,15 +20,22 @@ namespace Variate.Data
             {
                 modelBuilder.Entity<Category>().HasData(
                     new {Id = 1, Name = "Electronics"},
-                    new {Id = 2, Name = "Home and kitchen"},
-                    new {Id = 3, Name = "Fashion and beauty"},
-                    new {Id = 4, Name = "Toys and games"},
-                    new {Id = 5, Name = "Books and DVDs"},
-                    new {Id = 6, Name = "Baby Products"},
-                    new {Id = 7, Name = "Outdoor and sports equipment"},
-                    new {Id = 8, Name = "Health and wellness products"},
-                    new {Id = 9, Name = "Arts and craft supplies"},
-                    new {Id = 10, Name = "Musical Instruments"}
+                    new {Id = 2, Name = "Home and Kitchen"},
+                    new {Id = 3, Name = "Fashion and Beauty"},
+                    new {Id = 4, Name = "Musical Instruments"},
+                    new {Id = 5, Name = "Art and Crafts"},
+                    new {Id = 6, Name = "Baby and Toddler"},
+                    new {Id = 7, Name = "Bed and Bath"},
+                    new {Id = 8, Name = "Decor and Furniture"},
+                    new {Id = 9, Name = "Health and Beauty"},
+                    new {Id = 10, Name = "Home and Garden"},
+                    new {Id = 11, Name = "Jewellery and Watches"},
+                    new {Id = 12, Name = "Luggage and Travel"},
+                    new {Id = 13, Name = "Office and Stationery"},
+                    new {Id = 14, Name = "Pet Products"},
+                    new {Id = 15, Name = "Sports and Outdoor"},
+                    new {Id = 16, Name = "Tools and DIY"},
+                    new {Id = 17, Name = "Toys and Games"}
                 );
             }
         }
