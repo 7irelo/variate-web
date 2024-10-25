8a5499eb4d6b7f27d6bbbd82f4d0e2c23713631b
7irelo <tirelo.eric@gmail.com>
2024-10-25 11:53:18 +0200

    System.GC.Collect()

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -3,17 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using variate.Data;
 using variate.Models;
 using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace variate.Controllers
 {
     [Route("products")]
     public class ProductController : Controller
     {
+        private readonly ILogger<ProductController> _logger;
         private readonly ApplicationDbContext _db;
 
-        public ProductController(ApplicationDbContext db)
+        public ProductController(ILogger<ProductController> logger, ApplicationDbContext db)
         {
-            _db = db;
+            _logger = logger;
+            _db = db; 
         }
 
         // GET: /products
@@ -31,14 +34,16 @@ namespace variate.Controllers
                                                 p.Category.Name.ToLower().Contains(searchString));
             }
 
-            return View(products.ToList());
+            return View(products);
         }
 
         // GET: /products/details/5
-        [HttpGet("details/{id}")]
+        [HttpGet("{id}")]
         public ActionResult Details(int id)
         {
-            var product = _db.Products.Include(p => p.Category).FirstOrDefault(p => p.Id == id);
+            var product = _db.Products
+                                .Include(p => p.Category)
+                                .FirstOrDefault(p => p.Id == id);
 
             if (product == null)
             {
@@ -58,37 +63,78 @@ namespace variate.Controllers
         // POST: /products/create
         [HttpPost("create")]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Create(Product product)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (product.Name.Contains(product.Brand))
+                {
+                    ModelState.AddModelError("CustomError", "The Product Name should not contain the Brand Name");
+                }
+                else if (product.CategoryId < 1)
+                {
+                    ModelState.AddModelError("CustomError", "The Category ID cannot be less than zero");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    _db.Products.Add(product);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product); 
             }
             catch
             {
-                return View();
+                return View(product);
             }
         }
 
         // GET: /products/edit/5
         [HttpGet("edit/{id}")]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var product = _db.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         // POST: /products/edit/5
         [HttpPost("edit/{id}")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(Product product)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (product.Name.Contains(product.Brand))
+                {
+                    ModelState.AddModelError("CustomError", "The Product Name should not contain the Brand Name");
+                }
+                else if (product.CategoryId < 1)
+                {
+                    ModelState.AddModelError("CustomError", "The Category ID cannot be less than zero");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    _db.Products.Update(product);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product); 
             }
             catch
             {
-                return View();
+                return View(product);
             }
         }
 
@@ -102,11 +148,18 @@ namespace variate.Controllers
         // POST: /products/delete/5
         [HttpPost("delete/{id}")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteP(int id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var product = _db.Products.FirstOrDefault(p => p.Id == id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                
+                _db.Products.Remove(product);
+                return RedirectToAction("Index");
             }
             catch
             {
